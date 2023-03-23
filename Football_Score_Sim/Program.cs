609e8a312e85914e0ac33edd77b27eb3624a34f8
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2023-03-23 15:44:56 +0100

    fileReader connected to match result process

--- Football_Score_Sim/Program.cs
+++ Football_Score_Sim/Program.cs
@@ -4,42 +4,45 @@ using Football_Score_Sim;
 Team t1 = new Team("F.C. København", "FCK");
 Team t2 = new Team("FC Nordsjælland", "FCN");
 Team t3 = new Team("Lyngby Boldklub", "LBK");
-Team t4 = new Team("Silkeborg IF", "SIF");
+Team t4 = new Team("Viborg FF", "VFF");
+Team t5 = new Team("AGF", "AGF");
+Team t6 = new Team("Randers FC", "RFC");
+Team t7 = new Team("Brøndby IF", "BIF");
+Team t8 = new Team("Silkeborg IF", "SIF");
+Team t9 = new Team("FC Midtjylland", "FCM");
+Team t10 = new Team("OB", "OB");
+Team t11 = new Team("AC Horsens", "ACH");
+Team t12 = new Team("AaB", "AAB");
 
 var l1Teams = new List<Team>();
 l1Teams.Add(t1);
 l1Teams.Add(t2);
 l1Teams.Add(t3);
 l1Teams.Add(t4);
+l1Teams.Add(t5);
+l1Teams.Add(t6);
+l1Teams.Add(t7);
+l1Teams.Add(t8);
+l1Teams.Add(t9);
+l1Teams.Add(t10);
+l1Teams.Add(t11);
+l1Teams.Add(t12);
 
 League l1 = new League("Superliga", l1Teams);
 
-/*Console.WriteLine(t1.Name);
-Console.WriteLine(t2.Name);
-using(var reader = new StreamReader("scores.csv"))
+using(var reader = new StreamReader("scores/s2022-2023/r1.csv"))
 {
     while (!reader.EndOfStream)
     {
         var line = reader.ReadLine();
         var values = line.Split(',');
-        foreach (var team in l1.Teams)
-        {
-            // if (team.Abbr.Equals(values[0]))
-            // {
-            //     team.Matches = team.Matches + 1;
-            // }
-
-        }
-
-        Console.WriteLine(values[0]+ " - " + values[1] + " " + values[2] + ":" + values[3]);
-        
+        l1.AddMatch(values[0], values[1], Convert.ToByte(values[2]), Convert.ToByte(values[3]));
     }
-}*/
-// Console.WriteLine(t1.Name + t1.Matches);
+}
 
 var lines = "-------";
 
-l1.AddMatch("FCK", "FCN", 11, 8);
+//l1.AddMatch("FCK", "FCN", 11, 8);
 
 Console.WriteLine(l1.Teams[2].Name);
 Console.WriteLine(l1.Teams[2].Points);
@@ -55,18 +58,18 @@ Console.WriteLine(l1.Teams[3].GoalsAgainst);
 Console.WriteLine(l1.Teams[3].GoalDifference);
 Console.WriteLine(l1.Teams[3].Matches);
 
-l1.AddMatch("FCK", "FCN", 10, 10);
-
-Console.WriteLine(l1.Teams[2].Name);
-Console.WriteLine(l1.Teams[2].Points);
-Console.WriteLine(l1.Teams[2].GoalsFor);
-Console.WriteLine(l1.Teams[2].GoalsAgainst);
-Console.WriteLine(l1.Teams[2].GoalDifference);
-Console.WriteLine(l1.Teams[2].Matches);
-Console.WriteLine(lines);
-Console.WriteLine(l1.Teams[3].Name);
-Console.WriteLine(l1.Teams[3].Points);
-Console.WriteLine(l1.Teams[3].GoalsFor);
-Console.WriteLine(l1.Teams[3].GoalsAgainst);
-Console.WriteLine(l1.Teams[3].GoalDifference);
-Console.WriteLine(l1.Teams[3].Matches);
+// l1.AddMatch("FCK", "FCN", 10, 10);
+//
+// Console.WriteLine(l1.Teams[2].Name);
+// Console.WriteLine(l1.Teams[2].Points);
+// Console.WriteLine(l1.Teams[2].GoalsFor);
+// Console.WriteLine(l1.Teams[2].GoalsAgainst);
+// Console.WriteLine(l1.Teams[2].GoalDifference);
+// Console.WriteLine(l1.Teams[2].Matches);
+// Console.WriteLine(lines);
+// Console.WriteLine(l1.Teams[3].Name);
+// Console.WriteLine(l1.Teams[3].Points);
+// Console.WriteLine(l1.Teams[3].GoalsFor);
+// Console.WriteLine(l1.Teams[3].GoalsAgainst);
+// Console.WriteLine(l1.Teams[3].GoalDifference);
+// Console.WriteLine(l1.Teams[3].Matches);
