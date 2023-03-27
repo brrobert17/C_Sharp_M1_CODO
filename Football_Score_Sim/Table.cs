7d522a651543b9c73a64535e4334b7dca300342d
Arkitekt-s <sfattahieh@gmail.com>
2023-03-27 14:11:09 +0200

    table

--- Football_Score_Sim/Table.cs
+++ Football_Score_Sim/Table.cs
@@ -33,6 +33,7 @@ public static class Table
         l1Teams.Add(t10);
         l1Teams.Add(t11);
         l1Teams.Add(t12);
+        League l1 = new League("Superliga", l1Teams);
         
         IEnumerable<Tuple<int, string, string>> tableLG =
             new[]
@@ -40,7 +41,7 @@ public static class Table
                 //read teams from table
 
                 Tuple.Create(1, l1Teams[0].Name, l1Teams[0].Abbr),
-                /*Tuple.Create(2, l1Teams[1].Name, l1Teams[1].Abbr),
+                Tuple.Create(2, l1Teams[1].Name, l1Teams[1].Abbr),
                 Tuple.Create(3, l1Teams[2].Name, l1Teams[2].Abbr),
                 Tuple.Create(4, l1Teams[3].Name, l1Teams[3].Abbr),
                 Tuple.Create(5, l1Teams[4].Name, l1Teams[4].Abbr),
@@ -50,7 +51,7 @@ public static class Table
                 Tuple.Create(9, l1Teams[8].Name, l1Teams[8].Abbr),
                 Tuple.Create(10, l1Teams[9].Name, l1Teams[9].Abbr),
                 Tuple.Create(11, l1Teams[10].Name, l1Teams[10].Abbr),
-                Tuple.Create(12, l1Teams[11].Name, l1Teams[11].Abbr),*/
+                Tuple.Create(12, l1Teams[11].Name, l1Teams[11].Abbr),
 
 
 
